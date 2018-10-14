97202b2d44614f5fbf6b1aa07ffa29750db11108
Milan Harris <milanharrisdev@gmail.com>
2018-10-14 10:33:52 -0500

    Initial Files

---
+++ GolfAR/Assets/Scripts/ConnectionManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quobject.SocketIoClientDotNet.Client;
+using Newtonsoft.Json;
+
+public class Message
+{
+    public string id;
+    public string data;
+};
+
+public class ConnectionManager : MonoBehaviour {
+
+    public string serverURL = "http://localhost:3000";
+
+    protected Socket socket = null;
+	
+    private void Start()
+    {
+        DoOpen();
+    }
+	
+    private void OnDestroy()
+    {
+        DoClose();
+    }
+	
+    private void DoOpen()
+    {
+        if (socket == null)
+        {
+            socket = IO.Socket (serverURL);
+            socket.On("welcome", (data) =>
+            {
+                string str = data.ToString();
+	
+                Message message = JsonConvert.DeserializeObject<Message>(str);
+                string debugMessage = "user#" + message.id + ": " + message.data;
+                Debug.Log(debugMessage);
+            });
+        }
+    }
+	
+    private void DoClose()
+    {
+        if (socket != null)
+        {
+            socket.Disconnect();
+            socket = null;
+        }
+    }
+}
