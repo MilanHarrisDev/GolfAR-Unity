e1abde005e944daeef2467a8ef1fc01acb19503a
Milan Harris <milanharrisdev@gmail.com>
2018-10-14 11:59:48 -0500

    Arrow Pull Implemented

--- GolfAR/Assets/Scripts/ShootController.cs
+++ GolfAR/Assets/Scripts/ShootController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootController : MonoBehaviour {
 
@@ -10,15 +11,29 @@ public class ShootController : MonoBehaviour {
     private Transform ball;
     [SerializeField]
     private Transform arrowPivot;
+    [SerializeField]
+    private Image arrow;
 
-    void CastRayToWorld(){
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+            RotateArrowPivot(Input.mousePosition);
+        else if (Input.touchCount > 0)
+            RotateArrowPivot(Input.GetTouch(0).position);
+    }
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+    void RotateArrowPivot(Vector3 pos){
+        Debug.Log("rotating");
+
+        Ray ray = Camera.main.ScreenPointToRay(pos);
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, 100000f, shootMask))
         {
             arrowPivot.LookAt(ball.position + (GetXZVector(ball.position) - GetXZVector(hit.point)).normalized);
-            arrowPivot.rotation = Quaternion.Euler(0, arrowPivot.eulerAngles.y, 0);
+            arrowPivot.rotation = Quaternion.Euler(0, arrowPivot.eulerAngles.y + 90, 0);
+
+            float fill = Vector3.Distance(GetXZVector(ball.position), GetXZVector(hit.point)) / 5f;
+            arrow.fillAmount = (fill > 0.232f) ? fill : 0.232f;
         }
     }
 
